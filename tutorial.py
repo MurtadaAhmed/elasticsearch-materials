2fa009b69a4fafebfe933f809211acab73f1bf28
Murtada Ahmed <murtadhabg@gmail.com>
2024-05-01 21:30:29 +0300

    elasticsearch tutorial

--- tutorial.py
+++ tutorial.py
@@ -1,18 +1,62 @@
-from elasticsearch import Elasticsearch
-import io
-es = Elasticsearch("http://localhost:9200")
 
-print(es.ping())
 
-with io.open("test_index_names.txt", "r", encoding="utf-8") as f1:
-    data = f1.read()
-    f1.close()
+# use elastic search API
+import requests
+response = requests.get("http://localhost:9200/_cat/indices?format=json")
+data = response.json()
+[print(row["index"]) for row in data]
+# create index using elastic API
+create_index = requests.put("http://localhost:9200/test_index_using_api")
+print(create_index.text)
+# search index using API
+index_name = "library"
+search_result = requests.get("http://localhost:9200/" + index_name)
+print(search_result.text)
 
-data = data.split("\n")
 
-for index in data:
-    response = es.indices.create(index=index)
-    print(response)
+
+# from elasticsearch import Elasticsearch
+# import io
+# es = Elasticsearch("http://localhost:9200")
+#
+# print(es.ping())
+
+
+
+
+# delete indices from file:
+# with io.open("test_index_names.txt", "r", encoding="utf-8") as f1:
+#     data = f1.read()
+#     f1.close()
+# data = data.split("\n")
+# for index in data:
+#     response = es.indices.delete(index=index)
+#     print(response)
+
+
+# delete index
+# index = "movies"
+# response=es.indices.get_alias(index=index)
+# for index in response:
+#     es.indices.delete(index=index)
+
+
+# search elasticsearch index
+# index = "tutorial"
+# response=es.search(index=index)
+# print(response)
+
+
+# create index from .txt file
+# with io.open("test_index_names.txt", "r", encoding="utf-8") as f1:
+#     data = f1.read()
+#     f1.close()
+#
+# data = data.split("\n")
+#
+# for index in data:
+#     response = es.indices.create(index=index)
+#     print(response)
 
 
 # creating an index
