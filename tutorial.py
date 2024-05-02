15564fdb0980f767b1e5ce1a1596fc68487640d7
Murtada Ahmed <murtadhabg@gmail.com>
2024-05-02 19:00:51 +0300

    elasticsearch tutorial

--- tutorial.py
+++ tutorial.py
@@ -1,17 +1,70 @@
-
-
-# use elastic search API
 import requests
-response = requests.get("http://localhost:9200/_cat/indices?format=json")
-data = response.json()
-[print(row["index"]) for row in data]
-# create index using elastic API
-create_index = requests.put("http://localhost:9200/test_index_using_api")
-print(create_index.text)
-# search index using API
-index_name = "library"
-search_result = requests.get("http://localhost:9200/" + index_name)
-print(search_result.text)
+
+# find document using rest api
+url = "http://localhost:9200/"
+doc_id = "1"
+response = requests.get(url + "library/_doc/" + doc_id + "?_source=false&pretty")
+print(response.text)
+
+
+# add document in bulk
+# url = "http://localhost:9200/"
+# headers = {
+#     "content-type": "application/json",
+# }
+#
+#
+# with io.open("weblog.csv", "r", encoding="utf-8") as f1:
+#     data = f1.read()
+#     f1.close()
+#
+# lines = data.split("\n")[1:]
+#
+# i = 4
+# for line in lines:
+#     ip = line.split(",")[0]
+#     time = line.split(",")[1]
+#     url_link = line.split(",")[2]
+#     status = line.split(",")[3]
+#
+#     json_data = {
+#         "ip_address": ip,
+#         "time_stamp": time,
+#         "url": url_link,
+#         "status": status
+#     }
+#
+#     response = requests.put(url + "library/_doc/" + str(i) + "?pretty", headers=headers, json=json_data)
+#     print(f"doc number {i} is added")
+#     i += 1
+
+
+
+
+
+# add document using api
+# url = "http://localhost:9200/"
+# headers = {
+#     "content-type": "application/json"
+# }
+# json_data = {
+#     "message3": "this is test message3"
+# }
+# response = requests.put(url + "library/_doc/3?pretty", headers=headers, json=json_data)
+# print(response)
+
+# # use elastic search API
+# import requests
+# response = requests.get("http://localhost:9200/_cat/indices?format=json")
+# data = response.json()
+# [print(row["index"]) for row in data]
+# # create index using elastic API
+# create_index = requests.put("http://localhost:9200/test_index_using_api")
+# print(create_index.text)
+# # search index using API
+# index_name = "library"
+# search_result = requests.get("http://localhost:9200/" + index_name)
+# print(search_result.text)
 
 
 
